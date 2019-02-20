62daaf31a9068a395286a70d7ebc9fbe996f9d53
Chris Caudill <christopherscaudill@gmail.com>
2019-02-20 18:28:21 -0500

    ic

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,9 +4,37 @@ namespace Delegates
 {
     class Program
     {
+        delegate void mathfunc(); //creating a template for a method call, type for a method, treat methods more like data
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            mathfunc method;
+            //set method to print method
+            method = Print;
+            method();
+            method = Write;
+            method();
+
+            mathfunc[] mets = { Print, Write };
+            foreach (var i in mets)
+            {
+                i();
+            }
+        }
+
+        static void Print()
+        {
+            Console.WriteLine("Print");
+        }
+
+        static void Write()
+        {
+            Console.WriteLine("Write");
+        }
+
+        static void Calc(int i)
+        {
+
         }
     }
 }
